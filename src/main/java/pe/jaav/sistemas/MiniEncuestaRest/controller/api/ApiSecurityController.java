1e940ec0913a7aa0a23e4ed6241e4b2dcf99a201
Jose Arauco <araucovillar@gmail.com>
2018-10-14 04:02:52 -0500

    Fix security , etc

---
+++ src/main/java/pe/jaav/sistemas/MiniEncuestaRest/controller/api/ApiSecurityController.java
@@ -0,0 +1,25 @@
+package pe.jaav.sistemas.MiniEncuestaRest.controller.api;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import pe.jaav.sistemas.MiniEncuestaRest.model.RespuestaApi;
+
+
+@RestController
+@CrossOrigin
+@RequestMapping("api/security")
+public class ApiSecurityController {
+
+	@PostMapping(value="token", produces = MediaType.APPLICATION_JSON_VALUE)
+	public ResponseEntity<RespuestaApi> verificarToken(){
+		return new ResponseEntity<RespuestaApi>(
+				new RespuestaApi("OK",SecurityContextHolder.getContext().getAuthentication().getPrincipal()), HttpStatus.OK);
+	}
+}
---
+++ src/main/java/pe/jaav/sistemas/MiniEncuestaRest/model/RespuestaApi.java
@@ -0,0 +1,27 @@
+package pe.jaav.sistemas.MiniEncuestaRest.model;
+
+public class RespuestaApi{
+
+	private String status;
+	private Object body;
+	
+	public RespuestaApi() {}
+	
+	public RespuestaApi(String status, Object body) {
+		this.status = status;
+		this.body = body;
+	}
+	
+	public String getStatus() {
+		return status;
+	}
+	public void setStatus(String status) {
+		this.status = status;
+	}
+	public Object getBody() {
+		return body;
+	}
+	public void setBody(Object body) {
+		this.body = body;
+	}
+}
